c4db77b606118581300fad868ee48858c120ba60
Maruf Ahmed <marufdev09@gmail.com>
2025-05-08 00:57:51 +0600

    solved 2 problem

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
+{
+  const formatString = (input: string, toUpper?: boolean): string => {
+    let formattedString = "";
+    if (toUpper || toUpper == undefined) {
+      return input.toUpperCase();
+    } else if (!toUpper) {
+      return input.toLowerCase();
+    }
+  };
 
-const s:string = "Hello, world!";
+  console.log(formatString("Helllo", true));
+}
 
-console.log(s);
+{
+  type Tbook = {
+    title: string;
+    rating: number;
+  };
+
+  const books: Tbook[] = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+
+  const filterByRating = (items: Tbook[]): Tbook[] => {
+    const filterdbook = items.filter((item) => item.rating > 4);
+
+    return filterdbook;
+  };
+
+  console.log(filterByRating(books));
+}
+
+
+{
+
+
+
+
+}
